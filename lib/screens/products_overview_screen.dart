a36af6a90e8df7ccf71666024fc5f3483824ef74
Ivi Berberi <iviberberi@gmail.com>
2020-12-11 18:12:25 +0100

    Added http call & error handling.

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_eshop/providers/cart.dart';
+import 'package:my_eshop/providers/products.dart';
 import 'package:my_eshop/screens/cart_screen.dart';
 import 'package:my_eshop/widgets/app_drawer.dart';
 import 'package:my_eshop/widgets/badge.dart';
@@ -16,6 +17,33 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    // Provider.of<Products>(context).fetchAndSetProducts(); //WON'T WORK! (only if listen:false)
+    // Future.delayed(Duration.zero).then((_) => {
+    // Provider.of<Products>(context).fetchAndSetProducts();
+    // });
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,8 +67,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                         child: Text('Only Favorites'),
                         value: FilterOptions.Favorites),
                     PopupMenuItem(
-                        child: Text('Show All'),
-                        value: FilterOptions.All),
+                        child: Text('Show All'), value: FilterOptions.All),
                   ]),
           Consumer<Cart>(
             builder: (_, cart, ch) => Badge(
@@ -57,7 +84,11 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductsGrid(_showOnlyFavorites),
     );
   }
 }
